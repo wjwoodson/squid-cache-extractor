f45d4c758527c9d2979cdb10ede4a6804c123969
Will Woodson <wjwoodson@gmail.com>
2016-11-23 11:19:58 -0600

    stubbing out functions for cache_file parsing and insert to database

--- cache-extractor.py
+++ cache-extractor.py
@@ -2,13 +2,13 @@
 #
 
 import os
+import md5
 
 # set cache_dir from which to extract files
 cache_dir = "./data/squid3"
 
-print("cache_dir: '%s'" % cache_dir)
 
-# hex word parse of squid_meta, headers, payload from a cache_file
+# hex byte parse of squid_meta, headers, payload from a cache_file
 def parse_cache_file(cache_file):
 	with open(cache_file, 'rb') as cache_file_raw:
 		print(cache_file)
@@ -37,26 +37,50 @@ def parse_cache_file(cache_file):
 						byte = ""
 						str_buff = word2
 			
-			# append word to buffer and read another 2 bytes
+			# append byte to buffer and read another
 			str_buff = str_buff + byte
 			byte = cache_file_raw.read(1)
-		
+
+		md5sum = md5.new()
+		md5sum.update(payload)
+		print("payload md5: "+md5sum.hexdigest())		
+
 		# check for gzip file signatures
 		# hex bytes 1f8b08 = gzip signature
 		if payload.encode('hex')[:6] == "1f8b08":
 			decompress_gzip(payload)
+		
+		# return the parsed cache_file
+		return "==== parsed_cache_file data structure"
 
+# parse squid_meta in cache_file header
+def parse_squid_meta(squid_meta):
+	print("==== parsed squid meta ====")
 
 # decompress a gzipped response payload
 def decompress_gzip(response_payload):
 	print("==== payload is gzip ====")
 
+# decompress deflate/zip/PK response payload
+def decompress_pk(response_payload):
+	print("==== payload is zip ====")
+
+# parse out references from response_payload
+def parse_references(response_payload):
+	print("==== parsed references in payload ====")
+
+# insert record into database
+def insert_record(parsed_cache_file):
+	pass
 
 
 ##### TESTING #####
 parse_cache_file('./data/squid3/00/5D/00005D34')
 
 exit()
+
+# print out the configured cache_dir
+print("cache_dir: '%s'" % cache_dir)
 
 # get list of all cache_files
 cache_files = []
