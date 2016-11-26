5a0cdf091ad06a48552b73ad526a5dbb9df1374b
Will Woodson <wjwoodson@gmail.com>
2016-11-26 09:25:00 -0600

    fixed byte offset in parse_swap_state_entry

--- parse-swap-state.py
+++ parse-swap-state.py
@@ -2,6 +2,7 @@
 #
 import binascii
 
+# parse an individual 72 byte entry from swap.state
 def parse_swap_state_entry(swap_state_entry):
 
 	swap_state_parsed = {}
@@ -13,9 +14,9 @@ def parse_swap_state_entry(swap_state_entry):
 	lastmod = swap_state_entry[32:40] # lastmod (host endianness bytes)
 	swap_file_sz = swap_state_entry[40:47] # swap_file_sz (host endianness bytes)
 	refcount = swap_state_entry[48:49] # refcount (host endianness bytes)
-	flags = swap_state_entry[50:51] # flags (host endianness bytes)
+	flags = swap_state_entry[50:52] # flags (host endianness bytes)
 	cache_key = swap_state_entry[52:68] # md5 cache_key (hex string)
-
+	
 	swap_state_parsed['swap_filen'] = binascii.b2a_hex(swap_filen[::-1])
 	swap_state_parsed['meta_cache_key'] = binascii.b2a_hex(cache_key)
 	swap_state_parsed['meta_timestamp'] =  int(binascii.b2a_hex(timestamp[::-1]), 16)
@@ -26,13 +27,14 @@ def parse_swap_state_entry(swap_state_entry):
 	swap_state_parsed['meta_refcount'] = int(binascii.b2a_hex(refcount[::-1]), 16)
 	swap_state_parsed['meta_flags'] = int(binascii.b2a_hex(flags[::-1]), 16)
 
+	# return dictionary of fields parsed on ascii/decimal
 	return swap_state_parsed
 
 # set cache_dir from which to extract files
 cache_dir = "./data/squid3"
 
 # hex byte parse of squid_meta, headers, payload from a cache_file
-# open the cache_file on disk and read
+# open the cache_file on disk and read one enry ata time
 with open("%s/swap.state" % cache_dir, 'rb') as swap_state_raw:
 
                 # create dictionary to store all parsed fields
